2dc10712f8c5dde852c256219ca7207131d0860c
Uttom <uttom.teacher.gps2@gmail.com>
2022-10-11 10:33:14 +0600

    Blog page questions added

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -3,7 +3,18 @@ import React from 'react';
 const Blog = () => {
     return (
         <div>
-            I am blog
+             <div className='bg-dark text-white p-2 rounded-3'>
+                <h3>1. What is the purpose of React Router?</h3>
+                <p>React Router is a standard library for routing in React. It enables the navigation among views of various components in a React Application, allows changing the browser URL, and keeps the UI in sync with the URL.</p>
+            </div> <br />
+            <div className='bg-dark text-white p-2 rounded-3'>
+                <h3>2. How does context api work?</h3>
+                <p>The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.</p>
+            </div> <br />
+            <div className='bg-dark text-white p-2 rounded-3'>
+                <h3>3. Explain useRef Hook?</h3>
+                <p>The useRef Hook allows you to persist values between renders. It can be used to store a mutable value that does not cause a re-render when updated. It can be used to access a DOM element directly.</p>
+            </div> <br />
         </div>
     );
 };
