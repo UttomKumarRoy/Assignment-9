303e5e4af05fbd8ef347df19265e98006f0ac727
Uttom <uttom.teacher.gps2@gmail.com>
2022-10-11 10:57:54 +0600

    A chart is added

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { LineChart, Line, XAxis, YAxis, Tooltip } from 'recharts';
 
 const Statistics = () => {
+    const data = [
+        {
+            name: "React",
+            total: 8
+        },
+        {
+            name: "JavaScript",
+            total: 9
+        },
+        {
+            name: "CSS",
+            total: 8
+        },
+        {
+            name: "Git",
+            total: 11
+        }
+    ];
     return (
         <div>
-            I am Statistics
+            <LineChart width={500} height={400} data={data}>
+                <Line type="monotone" dataKey="total" stroke="#82ca9d" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip></Tooltip>
+            </LineChart>
         </div>
     );
 };
