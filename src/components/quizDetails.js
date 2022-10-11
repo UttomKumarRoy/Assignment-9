3b8ecdbd017b2e507f6f1236ca440cc89614706f
Uttom <uttom.teacher.gps2@gmail.com>
2022-10-11 10:06:01 +0600

    Quiz questions added

--- src/components/quizDetails.js
+++ src/components/quizDetails.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 const QuizDetails = (props) => {
     console.log(props);
+    const {question, id, options, correctAnswer}=props.data;
     return (
         <div>
-            I am quiz details
+            <div>{question}</div>
+            <br />
+            <div>{id}</div>
+            <div>{options.map(option  => <btn className="form-check-input mt-0" type="checkbox">{option}</btn>)}</div>
+            <div>{correctAnswer}</div>
+            <br />
+            <br />
         </div>
     );
 };
